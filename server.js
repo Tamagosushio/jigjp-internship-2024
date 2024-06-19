1e0d25b543bd42e10bbd184471374d13291cf2e4
tamagosushio <mitachi.ts75@gmail.com>
2024-06-19 23:15:16 +0900

    [add] 簡易的なしりとりを追加

--- server.js
+++ server.js
@@ -5,6 +5,28 @@ let previousWord = "しりとり";
 Deno.serve(async (request) => {
     const pathname = new URL(request.url).pathname;
     console.log(`pathname: ${pathname}`);
+    if(pathname === "/shiritori"){   
+        if(request.method === "GET"){
+            return new Response(previousWord);
+        }else if(request.method === "POST"){
+            const requestJson = await request.json();  // リクエストのペイロードを取得
+            const nextWord = requestJson["nextWord"];
+            if(previousWord.slice(-1) === nextWord.slice(0,1)){
+                previousWord = nextWord;
+            }else{
+                return new Response(
+                    JSON.stringify({
+                        "errorMessage": "しりとりが成立していません",
+                        "errorCode": "10001"
+                    }),{
+                        status: 400,
+                        headers: {"Content-Type": "application/json; charset=utf-8"}
+                    }
+                );
+            }
+            return new Response(previousWord);
+        }
+    }
     return serveDir(
         request, {
             fsRoot: "./public/",  // 公開フォルダを指定
